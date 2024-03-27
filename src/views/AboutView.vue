e2676aa592d8b63aeb1678624fed485f4357a22a
Rijvy <rrijvy@gmail.com>
2024-03-27 15:48:24 -0400

    wip

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,80 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container pt-8">
+    <div class="flex">
+      <img class="rounded" src="../assets/images/myself.jpeg" width="450" />
+      <div class="pl-16">
+        <p class="text-4xl pb-4">Myself, Ashikur Rahman Rijvy</p>
+        <p class="text-2xl">
+          Fascinated with electronics since childhood. From there, I studied in the Faculty of
+          Science, then studied computer science and engineering at the university, and now I have
+          established myself as a software engineer. I love to learn new things and implement them,
+          that's my passion. Although I am more curious about space but never got to know well due
+          to lack of sources. However, I have been working as a software engineer for the past 4
+          years in a reputed company in Bangladesh. When I'm not at the computer, I'm usually
+          reading, playing cricket or hanging out with my family and two cats.
+        </p>
+      </div>
+    </div>
+    <div class="pt-16 w-8/12 float-right">
+      <table class="table-exp text-2xl">
+        <tbody>
+          <tr>
+            <td><p class="text-white">July 2020 - July 2022</p></td>
+            <td>
+              <p class="text-3xl text-white">
+                Junior Software Developer at Alphasoft Technology Limited
+              </p>
+              <p>
+                Built ERP systems for Keya Cosmetics Ltd. in Bangladesh, focusing particularly on
+                the payroll system. Additionally, I maintain other systems such as POS and
+                attendance systems. Collaborate with a small group of talented individuals.
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><p class="text-white">July 2020 - July 2022</p></td>
+            <td>
+              <p class="text-3xl text-white">Associate Software Engineer at Kaz Software</p>
+              <p>
+                Build various kind of solutions mostly web based. Build tax related software. Doing
+                continuos development and continuos integration.
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td><p class="text-white">July 2022 - Present</p></td>
+            <td>
+              <p class="text-3xl text-white">Software Engineer at Kaz Software</p>
+              <p>
+                Working on robust applications, Built an ERP from scratch and designed it
+                efficiently. I implemented CI/CD using various AWS services. Additionally, I
+                developed a desktop application to ensure seamless synchronization with the web.
+              </p>
+              <p class="pt-2">
+                Lead the development and architecture of technical tools like content management
+                systems, REST APIs, and continuous integration pipelines to fulfill business
+                requirements.
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.table-exp {
+  tbody {
+    tr {
+      td {
+        vertical-align: top;
+        padding: 0px 0px 40px 0px;
+        &:nth-child(1) {
+          width: 230px;
+        }
+      }
+    }
+  }
+}
+</style>
