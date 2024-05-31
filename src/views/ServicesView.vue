d842d5e7aa33ec752fd7a2962620e3030c6d6ad2
rrijvy <32623510+rrijvy@users.noreply.github.com>
2024-06-01 01:27:01 +0600

    wip

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="container pt-10 font-family-titillium text-xl">
+  <div class="container pt-10 w-3/4 mx-auto font-family-titillium text-xl">
     <p>
       I offer a wide range of web development services, tailored to meet the unique needs of each
       project. As a full-stack web developer, I specialize in creating robust and scalable software
       solutions utilizing cutting-edge technologies. My expertise includes:
     </p>
-    <ul class="list-disc">
+    <ul class="mt-4 list-disc pl-10">
       <li><strong>Languages</strong>: C#, JavaScript, Python</li>
       <li><strong>Frameworks</strong>: React.js, Vue.js, Node.js, .NET Core, Django</li>
     </ul>
-    <p class="text-2xl font-medium">Services Offered:</p>
-    <ul class="list-disc">
+    <p class="mt-8 text-2xl font-medium">Services Offered:</p>
+    <ul class="mt-4 list-disc pl-10">
       <li>
         <strong>Custom Web Application Development</strong>: Crafting bespoke web applications that
         cater to your specific requirements.
@@ -24,7 +24,7 @@
         reliability, and scalability.
       </li>
     </ul>
-    <p>
+    <p class="mt-6">
       With extensive experience and a commitment to excellence, I bring your software ideas to life
       and help drive your business forward. Let's collaborate and transform your vision into
       reality.
