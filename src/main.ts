a1d2489cf4318bbfdc3216f4dff741cd9a34a643
rrijvy <rrijvy@gmail.com>
2024-05-31 19:30:42 +0600

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +7,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import {
+  faFacebookSquare,
+  faLinkedin,
+  faGithubSquare,
+  faNpm,
+  faSquareUpwork
+} from '@fortawesome/free-brands-svg-icons'
+
+config.familyDefault = 'classic'
+config.styleDefault = 'duotone'
+
+library.add(faCoffee)
+library.add(faFacebookSquare, faLinkedin, faGithubSquare, faNpm, faSquareUpwork)
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,3 +1,75 @@
+<script setup lang="tsx"></script>
 <template>
-  <div>TO DO</div>
+  <div>
+    <div class="absolute left-1/2 bottom-20 -translate-x-1/2 w-1/2 px-10">
+      <div class="flex items-center justify-between">
+        <a class="text-current" href="https://www.facebook.com/s.rijvy" target="_blank">
+          <font-awesome-icon icon="fa-brands fa-square-facebook" size="4x"
+        /></a>
+        <a class="text-current" href="https://www.facebook.com/s.rijvy" target="_blank"
+          ><font-awesome-icon icon="fa-brands fa-linkedin" size="4x"
+        /></a>
+        <a class="text-current" href="https://www.facebook.com/s.rijvy" target="_blank"
+          ><font-awesome-icon icon="fa-brands fa-square-github" size="4x"
+        /></a>
+        <a class="text-current" href="https://www.facebook.com/s.rijvy" target="_blank"
+          ><font-awesome-icon icon="fa-brands fa-npm" size="4x"
+        /></a>
+        <a class="text-current" href="https://www.facebook.com/s.rijvy" target="_blank"
+          ><font-awesome-icon icon="fa-brands fa-square-upwork" size="4x"
+        /></a>
+      </div>
+    </div>
+    <!-- <div class="text-center">
+      <p class="pt-5 text-2xl">
+        Ready to start your next project or have questions about my services? Don't hesitate to
+        reach out!
+      </p>
+      <div class="w-6/12 mx-auto pb-10">
+        <form class="text-start" action="" method="post">
+          <div class="md:flex items-center mt-12">
+            <div class="w-full md:w-1/2 flex flex-col">
+              <label class="leading-none text-white text-lg">Name</label>
+              <input
+                type="text"
+                class="leading-none text-white p-3 text-xl focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"
+              />
+            </div>
+            <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
+              <label class="leading-none text-white text-lg">Email</label>
+              <input
+                type="email"
+                class="leading-none text-white p-3 text-xl focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"
+              />
+            </div>
+          </div>
+          <div class="md:flex items-center mt-8">
+            <div class="w-full flex flex-col">
+              <label class="leading-none text-white text-lg">Subject</label>
+              <input
+                type="text"
+                class="leading-none text-white p-3 text-xl focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"
+              />
+            </div>
+          </div>
+          <div>
+            <div class="w-full flex flex-col mt-8">
+              <label class="leading-none text-white text-lg">Message</label>
+              <textarea
+                type="text"
+                class="h-40 leading-none text-white p-3 text-xl focus:outline-none focus:border-blue-700 mt-4 bg-gray-800 border-0 rounded"
+              ></textarea>
+            </div>
+          </div>
+          <div class="flex items-center justify-center w-full">
+            <button
+              class="mt-9 leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none"
+            >
+              Send message
+            </button>
+          </div>
+        </form>
+      </div>
+    </div> -->
+  </div>
 </template>
