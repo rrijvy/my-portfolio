74d380fafd187489cc96992b52bc2e76ebba4862
rrijvy <32623510+rrijvy@users.noreply.github.com>
2024-06-02 22:57:12 +0600

    update

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,3 +1,139 @@
 <template>
-  <div>TO DO</div>
+  <div
+    class="container mt-16 font-family-titillium flex flex-wrap flex-row justify-between sm:justify-center sm:px-2"
+  >
+    <div
+      class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w_32 sm:w-full"
+    >
+      <a href="#">
+        <img class="rounded-t-lg object-fill" src="../assets/images/rx-pdf-viewer.jpg" alt="" />
+      </a>
+      <div class="p-5">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            A simple pdf viewer for React.
+          </h5>
+        </a>
+        <p class="mb-3 text-gray-700 dark:text-gray-400">
+          <strong>rx-pdf-viewer</strong> is a lightweight PDF viewer for React that allows text
+          selection if available. It leverages the <strong>pdfjs-dist</strong> library under the
+          hood to render PDFs.
+        </p>
+        <a
+          href="https://github.com/rrijvy/rx-pdf-viewer"
+          target="_blank"
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div
+      class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w_32 sm:w-full sm:mt-5"
+    >
+      <a href="#">
+        <img class="rounded-t-lg object-fill" src="../assets/images/create-ftp-server.png" alt="" />
+      </a>
+      <div class="p-5">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            A simple FTP server for you.
+          </h5>
+        </a>
+        <p class="mb-3 text-gray-700 dark:text-gray-400">
+          Download the executable from github
+          <a href="https://github.com/rrijvy/create_ftp_server/releases/tag/1.0.1" target="_blank">
+            (link) </a
+          >. Just execute and enjoy.
+          <strong class="block">Username: root, Password: 456456</strong>
+        </p>
+        <a
+          href="https://github.com/rrijvy/create_ftp_server/releases/tag/1.0.1"
+          target="_blank"
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div
+      class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w_32 sm:w-full sm:mt-5"
+    >
+      <a href="#">
+        <img
+          class="rounded-t-lg object-fill"
+          src="../assets/images/colorful-copy-manager.png"
+          alt=""
+        />
+      </a>
+      <div class="p-5">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Colorful Copy Manager
+          </h5>
+        </a>
+        <p class="mb-3 text-gray-700 dark:text-gray-400">
+          Download <strong>dist</strong> from github
+          <a
+            href="https://github.com/rrijvy/colorful-copy-manager/releases/tag/1.0.1"
+            target="_blank"
+          >
+            (link) </a
+          >. Just load on chrome devtool and enjoy.
+        </p>
+        <a
+          href="https://github.com/rrijvy/colorful-copy-manager/releases/tag/1.0.1"
+          target="_blank"
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>TO DO</div>
+  <div class="container">
+    <div
+      class="absolute left-1/2 bottom-20 -translate-x-1/2 w-6/12 sm:w-11/12 text-3xl sm:text-lg flex justify-between items-center rounded-sm outline outline-offset-8"
+    >
+      <p>Download or Preview Resume</p>
+      <a
+        class="border rounded-md green py-2 px-5"
+        href="https://drive.google.com/file/d/1aceXF99gpf2qfc_WdjhkE9rGCfjmjnPf/view?usp=sharing"
+        target="_blank"
+        >Preview</a
+      >
+    </div>
+  </div>
 </template>
